9250f5f4899b257bf7dfce5f5993eb842a3f8bd0
madhu surisetti <flyabovetheclouds.rf@madhus-MacBook-Air.local>
2020-08-10 13:27:27 +0530

    migrating

---
+++ hookup.py
@@ -0,0 +1,11 @@
+# this script will create a yaml file containing all the hooks
+
+
+list_file = 'list.txt'
+
+# create a yaml template file
+with open(list_file, 'w') as l:
+    for num in range(101, 1000):
+        l.write(f"num: {num}\nlink: 
+# take each word/line from the 'list.txt' file
+# put that word in its proper place in 'vancamp.yml'
